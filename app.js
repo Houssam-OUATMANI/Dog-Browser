6a6b16c6738c280553f0704e3d4bd3403c6e5632
Houssam OUATMANI <proservicesplus4@gmail.com>
2020-09-01 23:13:00 +0200

    Dog Random Images with Fetch and Bootstrap Vannila JavaScript

---
+++ app.js
@@ -0,0 +1,25 @@
+const API_URI = "https://dog.ceo/api/breeds/image/random/50";
+const cardHolder = document.querySelector(".card-holder");
+const spinner = document.querySelector(".spinner-border")
+
+
+async function getDogs() {
+
+  spinner.style.display = ''  
+  cardHolder.innerHTML = "";
+  const response = await fetch(API_URI);
+  const data = await response.json();
+
+  data.message.map(
+    (dog) =>
+      (cardHolder.innerHTML += `
+    <div class="card" style="width: 20%;">
+        <img src="${dog}" class="card-img-top" alt="...">
+    </div>
+    `)
+  );
+
+  spinner.style.display = 'none'
+}
+
+getDogs();
